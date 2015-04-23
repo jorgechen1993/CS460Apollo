761af46af7665adc8aa635e05f19ac2521b35f69
chengua_jorg <chengua_jorg@141.133.165.227>
2015-04-23 14:28:54 -0400

    -another test

--- CS460ApolloPubCrawl/src/com/example/cs460apollopubcrawl/gitHubTest.java
+++ CS460ApolloPubCrawl/src/com/example/cs460apollopubcrawl/gitHubTest.java
@@ -1,7 +1,8 @@
 package com.example.cs460apollopubcrawl;
 
 public class gitHubTest {
-	//I modified this comment on my mac eclipse
+	//I remodified this comment on my mac eclipse
+	//added this comment
 	
 	//added a constructor
 	public gitHubTest(){}
